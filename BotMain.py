0bd56178e1cbc281ee9b8e0a2d1ba91b97278055
kelarus <vladislavnikwork@gmail.com>
2021-10-22 15:57:10 +0200

    Refactoring BotMain

--- BotMain.py
+++ BotMain.py
@@ -1,8 +1,9 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from BotHandler import dp
-
 from Client import ClientHandler
-from Server import ServerHandler
+
 
 ClientHandler.register_handler_client(dp)
 executor.start_polling(dp, skip_updates=True)
