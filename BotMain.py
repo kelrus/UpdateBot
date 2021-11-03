dff77da3593fe101b14c87f2e287246db9bacde9
kelarus <vladislavnikwork@gmail.com>
2021-11-03 17:25:08 +0200

    Created users db

--- BotMain.py
+++ BotMain.py
@@ -1,14 +1,18 @@
 from aiogram.utils import executor
 from BotHandler import Dp
 from Client import ClientHandler
+from Server import DataBase
 import BotHandler
 import asyncio
 
+
 async def scheduler():
-    BotHandler.Scheduler.start();
+    BotHandler.Scheduler.start()
 
 async def on_startup(_):
     asyncio.create_task(scheduler())
 
 ClientHandler.register_handler_client()
+DataBase.sql_start()
+
 executor.start_polling(Dp, skip_updates=True, on_startup=on_startup)
